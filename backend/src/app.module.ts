6ea7ef17c44a4a879bcf763fb1cf2f13c13f0e42
Angelo Marcos <64624199+AngeloMarcos@users.noreply.github.com>
2025-07-07 02:27:30 +0000

    sdafgsd

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,20 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectsModule } from './projects/projects.module';
 import { LeadsModule } from './leads/leads.module';
-import { ConfigModule } from '@nestjs/config';
-import { WebhookModule }  from './webhook/webhook.module';
+import { WebhookModule } from './webhook/webhook.module';
+
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: process.env.DATABASE_URL || 'data/anglotech.sqlite',
+      database: 'data/anglotech.sqlite',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
     ProjectsModule,
-    LeadsModule, 
+    LeadsModule,
     WebhookModule,
   ],
 })
